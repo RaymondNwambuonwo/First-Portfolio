a5077a5dbb182bb0c9f04df8197c3ec4af78e5ae
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-24 23:51:01 -0500

    added modals but need fixes

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -7,36 +7,36 @@ import OTOI from "../Images/OTOI.gif";
 import MemoryMatch from "../Images/memory.gif";
 import Pong from "../Images/pong.gif";
 import Modal from "react-bootstrap/Modal";
+// import ModalBody from "react-bootstrap/ModalBody";
+// import ModalHeader from "react-bootstrap/ModalHeader";
+// import ModalFooter from "react-bootstrap/ModalFooter";
+// import ModalTitle from "react-bootstrap/ModalTitle";
 import Button from "react-bootstrap/Button";
 
 function Projects() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const showModal = () => {
-    setIsOpen(true);
-  };
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-  const hideModal = () => {
-    setIsOpen(false);
-  };
   return (
     <div className="decker">
       <CardDeck>
-        <Card className="text-center cards" border="info">
+        <Card className="text-center" border="primary">
           <Card.Img className="carder" variant="top" src={OTOI} />
           <Card.Body>
             <Card.Title>OTOI</Card.Title>
             <Card.Text>
-              <Button variant="primary" size="sm" active onClick={showModal}>
+              <Button variant="primary" size="sm" active onClick={handleShow}>
                 More Info
               </Button>
-              <Modal show={isOpen} onHide={hideModal}>
-                <Modal.Header>
-                  <Modal.Title>Hi</Modal.Title>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title id="contained-modal-title">ffffff</Modal.Title>
                 </Modal.Header>
-                <Modal.Body></Modal.Body>
+                <Modal.Body>sdfdfsrff</Modal.Body>
                 <Modal.Footer>
-                  <Button className="bg-primary" size="sm" onClick={hideModal}>
+                  <Button variant="secondary" onClick={handleClose}>
                     Close
                   </Button>
                 </Modal.Footer>
@@ -45,63 +45,84 @@ function Projects() {
           </Card.Body>
         </Card>
 
-        <Card className="text-center">
+        <Card className="text-center cards" border="primary">
           <Card.Img className="carder" variant="top" src={Rolodex} />
           <Card.Body>
             <Card.Title>Rolodex</Card.Title>
             <Card.Text>
-              <Button variant="info" size="sm" active onClick={showModal}>
+              <Button variant="primary" size="sm" active onClick={handleShow}>
                 More Info
               </Button>
-              <Modal show={isOpen} onHide={hideModal}>
-                <Modal.Header>
-                  <Modal.Title>Hi</Modal.Title>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body></Modal.Body>
+                <Modal.Body>
+                  Woohoo, you're reading this text in a modal!
+                </Modal.Body>
                 <Modal.Footer>
-                  <button onClick={hideModal}>Close</button>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
                 </Modal.Footer>
               </Modal>
             </Card.Text>
           </Card.Body>
         </Card>
 
-        <Card className="text-center">
+        <Card className="text-center cards" border="primary">
           <Card.Img className="carder" variant="top" src={MemoryMatch} />
           <Card.Body>
             <Card.Title>Memory Match</Card.Title>
             <Card.Text>
-              <Button variant="info" size="sm" active onClick={showModal}>
+              <Button variant="primary" size="sm" active onClick={handleShow}>
                 More Info
               </Button>
-              <Modal show={isOpen} onHide={hideModal}>
-                <Modal.Header>
-                  <Modal.Title>Hi</Modal.Title>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body></Modal.Body>
+                <Modal.Body>
+                  Woohoo, you're reading this text in a modal!
+                </Modal.Body>
                 <Modal.Footer>
-                  <button onClick={hideModal}>Close</button>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
                 </Modal.Footer>
               </Modal>
             </Card.Text>
           </Card.Body>
         </Card>
 
-        <Card className="text-center">
+        <Card className="text-center cards" border="primary">
           <Card.Img className="carder" variant="top" src={Pong} />
           <Card.Body>
             <Card.Title>Pong</Card.Title>
             <Card.Text>
-              <Button variant="info" size="sm" active onClick={showModal}>
+              <Button variant="primary" size="sm" active onClick={handleShow}>
                 More Info
               </Button>
-              <Modal show={isOpen} onHide={hideModal}>
-                <Modal.Header>
-                  <Modal.Title>Hi</Modal.Title>
+              <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>Modal heading</Modal.Title>
                 </Modal.Header>
-                <Modal.Body></Modal.Body>
+                <Modal.Body>
+                  Woohoo, you're reading this text in a modal!
+                </Modal.Body>
                 <Modal.Footer>
-                  <button onClick={hideModal}>Close</button>
+                  <Button variant="secondary" onClick={handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={handleClose}>
+                    Save Changes
+                  </Button>
                 </Modal.Footer>
               </Modal>
             </Card.Text>
