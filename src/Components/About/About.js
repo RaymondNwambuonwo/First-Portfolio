01e81fe690223a76da03042ce7e5c34816efa9e4
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-25 02:20:22 -0500

    modals are working and fixed

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,12 +1,53 @@
 import React, { Component } from "react";
 import "./About.css";
+import { Modal } from "antd";
+import Button from "react-bootstrap/Button";
+import "antd/dist/antd.css";
 
 class About extends Component {
+  state = { visible: false };
+
+  showModal = () => {
+    this.setState({
+      visible: true
+    });
+  };
+
+  handleOk = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
+
+  handleCancel = e => {
+    console.log(e);
+    this.setState({
+      visible: false
+    });
+  };
   render() {
     return (
       <div>
         <h1>Who is Raymond Nwambuonwo</h1>
-        <div></div>
+        <div>
+          <Button className="primary" onClick={this.showModal}>
+            Open Modal
+          </Button>
+          <Modal
+            title="Basic Modal"
+            visible={this.state.visible}
+            onOk={this.handleOk}
+            onCancel={this.handleCancel}
+            footer={[
+              <Button key="back" onClick={this.handleCancel}>
+                Ok
+              </Button>
+            ]}
+          >
+            <p>Some contents...</p>
+          </Modal>
+        </div>
 
         <div></div>
 
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
 import "./Projects.css";
 import Card from "react-bootstrap/Card";
 import CardDeck from "react-bootstrap/CardDeck";
@@ -6,131 +6,175 @@ import Rolodex from "../Images/Rolodex.gif";
 import OTOI from "../Images/OTOI.gif";
 import MemoryMatch from "../Images/memory.gif";
 import Pong from "../Images/pong.gif";
-import Modal from "react-bootstrap/Modal";
-// import ModalBody from "react-bootstrap/ModalBody";
-// import ModalHeader from "react-bootstrap/ModalHeader";
-// import ModalFooter from "react-bootstrap/ModalFooter";
-// import ModalTitle from "react-bootstrap/ModalTitle";
+import { Modal } from "antd";
 import Button from "react-bootstrap/Button";
+import "antd/dist/antd.css";
 
-function Projects() {
-  const [show, setShow] = useState(false);
+class Projects extends Component {
+  state = { visible: false };
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  state = {
+    modal1Visible: false,
+    modal2Visible: false,
+    modal3Visible: false,
+    modal4Visible: false
+  };
 
-  return (
-    <div className="decker">
-      <CardDeck>
-        <Card className="text-center" border="primary">
-          <Card.Img className="carder" variant="top" src={OTOI} />
-          <Card.Body>
-            <Card.Title>OTOI</Card.Title>
-            <Card.Text>
-              <Button variant="primary" size="sm" active onClick={handleShow}>
-                More Info
-              </Button>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title id="contained-modal-title">ffffff</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>sdfdfsrff</Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+  setModal1Visible(modal1Visible) {
+    this.setState({ modal1Visible });
+  }
 
-        <Card className="text-center cards" border="primary">
-          <Card.Img className="carder" variant="top" src={Rolodex} />
-          <Card.Body>
-            <Card.Title>Rolodex</Card.Title>
-            <Card.Text>
-              <Button variant="primary" size="sm" active onClick={handleShow}>
-                More Info
-              </Button>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  Woohoo, you're reading this text in a modal!
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+  setModal2Visible(modal2Visible) {
+    this.setState({ modal2Visible });
+  }
 
-        <Card className="text-center cards" border="primary">
-          <Card.Img className="carder" variant="top" src={MemoryMatch} />
-          <Card.Body>
-            <Card.Title>Memory Match</Card.Title>
-            <Card.Text>
-              <Button variant="primary" size="sm" active onClick={handleShow}>
-                More Info
-              </Button>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  Woohoo, you're reading this text in a modal!
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-            </Card.Text>
-          </Card.Body>
-        </Card>
+  setModal3Visible(modal3Visible) {
+    this.setState({ modal3Visible });
+  }
 
-        <Card className="text-center cards" border="primary">
-          <Card.Img className="carder" variant="top" src={Pong} />
-          <Card.Body>
-            <Card.Title>Pong</Card.Title>
-            <Card.Text>
-              <Button variant="primary" size="sm" active onClick={handleShow}>
-                More Info
-              </Button>
-              <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                  <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                  Woohoo, you're reading this text in a modal!
-                </Modal.Body>
-                <Modal.Footer>
-                  <Button variant="secondary" onClick={handleClose}>
-                    Close
-                  </Button>
-                  <Button variant="primary" onClick={handleClose}>
-                    Save Changes
-                  </Button>
-                </Modal.Footer>
-              </Modal>
-            </Card.Text>
-          </Card.Body>
-        </Card>
-      </CardDeck>
-    </div>
-  );
+  setModal4Visible(modal4Visible) {
+    this.setState({ modal4Visible });
+  }
+
+  render() {
+    return (
+      <div className="decker">
+        <CardDeck>
+          <Card className="text-center cards" border="primary">
+            <Card.Img className="carder" variant="top" src={OTOI} />
+            <Card.Body>
+              <Card.Title>OTOI</Card.Title>
+              <Card.Text>
+                <Button
+                  className="primary"
+                  onClick={() => this.setModal1Visible(true)}
+                >
+                  More Info
+                </Button>
+                <Modal
+                  title="OTOI"
+                  centered
+                  visible={this.state.modal1Visible}
+                  onOk={() => this.setModal1Visible(false)}
+                  onCancel={() => this.setModal1Visible(false)}
+                >
+                  <div className="text-center modtex">
+                    <h1>Description</h1>
+                    <p>
+                      OTOI is an application that allows you to keep track of
+                      information and to have it all in a central place. You are
+                      able to track your skills, agendas, and applications.
+                    </p>
+                    <h1>Technologies</h1>
+                    <p>Express, MongoDB, Node.js, React.</p>
+                  </div>
+                </Modal>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="text-center cards" border="primary">
+            <Card.Img className="carder" variant="top" src={Rolodex} />
+            <Card.Body>
+              <Card.Title>Rolodex</Card.Title>
+              <Card.Text>
+                <Button
+                  className="primary"
+                  onClick={() => this.setModal2Visible(true)}
+                >
+                  More Info
+                </Button>
+                <Modal
+                  title="OTOI"
+                  centered
+                  visible={this.state.modal2Visible}
+                  onOk={() => this.setModal2Visible(false)}
+                  onCancel={() => this.setModal2Visible(false)}
+                >
+                  <div className="text-center modtex">
+                    <h4>Description</h4>
+                    <p>
+                      OTOI is an application that allows you to keep track of
+                      information and to have it all in a central place. You are
+                      able to track your skills, agendas, and applications.
+                    </p>
+                    <h5>Technologies</h5>
+                    <p>Express, MongoDB, Node.js, React.</p>
+                  </div>
+                </Modal>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="text-center cards" border="primary">
+            <Card.Img className="carder" variant="top" src={MemoryMatch} />
+            <Card.Body>
+              <Card.Title>Memory Match</Card.Title>
+              <Card.Text>
+                <Button
+                  className="primary"
+                  onClick={() => this.setModal3Visible(true)}
+                >
+                  More Info
+                </Button>
+                <Modal
+                  title="OTOI"
+                  centered
+                  visible={this.state.modal3Visible}
+                  onOk={() => this.setModal3Visible(false)}
+                  onCancel={() => this.setModal3Visible(false)}
+                >
+                  <div className="text-center modtex">
+                    <h4>Description</h4>
+                    <p>
+                      OTOI is an application that allows you to keep track of
+                      information and to have it all in a central place. You are
+                      able to track your skills, agendas, and applications.
+                    </p>
+                    <h5>Technologies</h5>
+                    <p>Express, MongoDB, Node.js, React.</p>
+                  </div>
+                </Modal>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card className="text-center cards" border="primary">
+            <Card.Img className="carder" variant="top" src={Pong} />
+            <Card.Body>
+              <Card.Title>Pong</Card.Title>
+              <Card.Text>
+                <Button
+                  className="primary"
+                  onClick={() => this.setModal4Visible(true)}
+                >
+                  More Info
+                </Button>
+                <Modal
+                  title="OTOI"
+                  centered
+                  visible={this.state.modal4Visible}
+                  onOk={() => this.setModal4Visible(false)}
+                  onCancel={() => this.setModal4Visible(false)}
+                >
+                  <div className="text-center modtex">
+                    <h4>Description</h4>
+                    <p>
+                      OTOI is an application that allows you to keep track of
+                      information and to have it all in a central place. You are
+                      able to track your skills, agendas, and applications.
+                    </p>
+                    <h5>Technologies</h5>
+                    <p>Express, MongoDB, Node.js, React.</p>
+                  </div>
+                </Modal>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </CardDeck>
+      </div>
+    );
+  }
 }
 
 export default Projects;
