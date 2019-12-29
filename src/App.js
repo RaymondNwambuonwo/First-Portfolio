c133e7e0803880c1d18df12af7bd241122371a1c
RaymondNwambuonwo <Raymondnwambuonwogriffin@gmail.com>
2019-12-29 00:00:26 -0500

    footer fix

--- src/App.js
+++ src/App.js
@@ -18,45 +18,45 @@ library.add(fab, far, fas);
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Navy />
-        <br />
-        <Route path="/" exact component={Home} />
-        <Route path="/About" component={About} />
-        <Route path="/Resume" component={Resume} />
+    <div className="containing">
+      <div className="pagecontent">
+        <Router>
+          <Navy />
+          <br />
+          <Route path="/" exact component={Home} />
+          <Route path="/About" component={About} />
+          <Route path="/Resume" component={Resume} />
 
-        <Route path="/Projects" exact component={Projects} />
-        <Route path="/Contact" component={Contact} />
-        <footer className="footy">
-          <div className="contain">
-            <a
-              className="iconic"
-              href="https://www.linkedin.com/in/raymondnwambuonwo/"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={["fab", "linkedin"]} />
-            </a>
-            <a
-              className="iconic"
-              href="https://github.com/RaymondNwambuonwo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={["fab", "github"]} />
-            </a>
-            <a
-              className="iconic"
-              href="https://medium.com/@raymondnwambuonwo"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <FontAwesomeIcon icon={["fab", "medium"]} />
-            </a>
-          </div>
-        </footer>
-      </Router>
+          <Route path="/Projects" exact component={Projects} />
+          <Route path="/Contact" component={Contact} />
+        </Router>
+      </div>
+      <footer className="footy">
+        <a
+          className="iconic"
+          href="https://www.linkedin.com/in/raymondnwambuonwo/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={["fab", "linkedin"]} />
+        </a>
+        <a
+          className="iconic"
+          href="https://github.com/RaymondNwambuonwo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={["fab", "github"]} />
+        </a>
+        <a
+          className="iconic"
+          href="https://medium.com/@raymondnwambuonwo"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FontAwesomeIcon icon={["fab", "medium"]} />
+        </a>
+      </footer>
     </div>
   );
 }
